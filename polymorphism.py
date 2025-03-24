d58c28656e18c5440c94e4f72936e5b5cb3c4104
Taruntkr2025 <ravipatitarun3@gmail.com>
2025-03-24 10:45:41 +0530

    Assignments

--- polymorphism.py
+++ polymorphism.py
@@ -1,19 +1,34 @@
 #polymorphism
 
 class Animal():
+
     def sound(self):
         return "Animal sound"
 
 class Dog(Animal):
+
     def sound(self):
         return "Dog will bark"
+    
+    def typeofAnimal(self):
+        return "This is a 4 legged Animal"
 
-class cat(Animal):
+class Cat(Dog,Animal):
+
     def sound(self):
         return "Cat Says Meow"
 
+
 class Human(Animal):
+
+    def sound(self):
+        return "Human Speaks"
     pass
 
-cat_Instance=cat()
+"""
+cat_Instance=Cat()
 print(cat_Instance.sound())
+print(cat_Instance.typeofAnimal())
+"""
+human=Human()
+print(human.sound())
